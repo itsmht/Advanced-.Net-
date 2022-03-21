618a9217985ad1a99518342048777aaf80c0e9a9
itsmht <50614875+itsmht@users.noreply.github.com>
2022-03-21 20:53:13 +0600

    Web API CRUD

---
+++ IntroWebAPI/IntroWebAPI/Controllers/StudentController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using IntroWebAPI.Models.Entity;
+using IntroWebAPI.Models.Database;
+
+namespace IntroWebAPI.Controllers
+{
+    public class StudentController : ApiController
+    {
+        [Route("api/student/add")]
+        [HttpPost]
+        public string Post(Student s)
+        {
+            UniEntities db = new UniEntities();
+            db.Students.Add(s);
+            db.SaveChanges();
+            return "Added";
+        }
+        [Route("api/student/get")]
+        public List<Student> Get()
+        {
+            UniEntities db = new UniEntities();
+            var data = db.Students.ToList();
+            return data;
+        }
+        [Route("api/student/get")]
+        public Student Get(int id)
+        {
+            var db = new UniEntities();
+            var data = (from s in db.Students
+                        where s.id == id
+                        select s).FirstOrDefault();
+            return data;
+        }
+        [Route("api/student/put")]
+        [HttpGet]
+        public string Put(int id, Student st)
+        {
+            var db = new UniEntities();
+            var data = (from s in db.Students
+                        where s.id == id
+                        select s).FirstOrDefault();
+            db.Students.Add(data);
+            db.Entry(data).CurrentValues.SetValues(st);
+            db.SaveChanges();
+            return "Updated";
+
+        }
+        [Route("api/student/delete")]
+        public string Delete(int id)
+        {
+            var db = new UniEntities();
+            var data = (from s in db.Students
+                        where s.id == id
+                        select s).FirstOrDefault();
+            db.Students.Remove(data);
+            db.SaveChanges();
+            return "Deleted";
+        }
+        
+
+    }
+}
---
+++ IntroWebAPI/IntroWebAPI/Models/Entity/StudentModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IntroWebAPI.Models.Entity
+{
+    public class StudentModel
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public System.DateTime DOB { get; set; }
+        public double CGPA { get; set; }
+
+    }
+}
